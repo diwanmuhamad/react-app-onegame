190ab471a3a80a70b01548aa2b5c3936ef0d88dc
diwanmuhamad <diwanmuhamad37@gmail.com>
2022-07-08 09:52:55 +0800

    EditThings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [tries, setTries] = useState(false);
+
+  useEffect(() => {
+    console.log("When mounting");
+
+    return () => {
+      console.log("when unmounting");
+    }
+  },[])
+
   return (
-    <div className="App">
-      Diwan Handsome
+    <div style={{cursor: "pointer"}} onClick={() => {setTries(!tries)}} className="App">
+      Diwan wonderful
     </div>
   );
 }
